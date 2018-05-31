2eb28e0da28b93fb25b5998ba3e962b16275b3a6
samirakia <ali@alis-Air.localdomain>
2018-05-31 16:47:09 -0700

    first welcome page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Welcome from './pages/welcome'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    <h1>Table Tonight!</h1>
+        <h1></h1>
+        <Welcome />
       </div>
     );
   }
---
+++ src/pages/welcome.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './welcome.css'
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        {/* Hello world */}
+        <div className="awesome" style={{border: '1px solid red'}}></div>
+        <h1>Welcome to Table Tonight!</h1>
+        <p>Resturants</p>
+
+        <section>
+            <div className="restaurant-card">
+                <div className="card-image">
+                    <img src="/assets/images/picture.jpeg" />
+                </div>
+
+                <div className="card-title">
+                    <h3>Resturant 1</h3>
+                    <p>Italian</p>
+                </div>
+
+                 <button onclick="myFunction()">Make Reservation here!</button>
+            </div>
+            <div className="restaurant-card">
+
+                <div className="card-image">
+                    <img src="/assets/images/pic3.jpg" />
+                </div>
+
+                <div className="card-title">
+                    <h3>Resturant 2</h3>
+                    <p>Sushi</p>
+                </div>
+
+                <div>
+                 <button onclick="myFunction()">Make Reservation here!</button>
+                </div>
+
+            </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Welcome
