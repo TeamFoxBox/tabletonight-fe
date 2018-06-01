4ac85ffdfd129c13deb457e438914954039c97c9
tinateenguyen <ttn.tinanguyen@gmail.com>
2018-06-01 14:44:13 -0700

    Registration data passes to users database

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,18 @@
-const BASE = 'http://localhost:3001'
+const BASE = 'http://localhost:3000'
 
 let createUser = function(user) {
   let newUser = {user: user}
-  return fetch(BASE+'/register', {
+  return fetch(BASE+'/users', {
     body: JSON.stringify(newUser),
-    header: {
+    headers: {
       'Content-Type': 'application/json'
     },
     method: "POST"
   })
+  .then((rawResponse) => {
+    let parsedResponse = rawResponse.json()
+    return parsedResponse
+  })
 }
+
 export { createUser }
