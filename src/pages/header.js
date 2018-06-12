f180a16ccb4b8f4bdfce564e69be5484ceb7b00b
Damon Lemmons <damonlemmons@Damons-Air.localdomain>
2018-06-11 17:13:53 -0700

    changed bootstrap theme and nav bar

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import reactDOM, {render} from 'react-dom'
 import {
 	Button,
 	Col,
 	ControlLabel,
 	FormGroup,
 	FormControl,
-	Row
+	Row, Navbar, Nav, NavItem, NavDropdown, MenuItem, NavbarHeader
 } from 'react-bootstrap'
 import AuthService from '../services/AuthService'  // <- We use the AuthService to logout
 import { withRouter } from 'react-router-dom'
+
 
 
 const Auth = new AuthService() // <- Create a new instance of the Auth service
@@ -23,45 +25,38 @@ class Header extends Component {
     render() {
 
         return(
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <a class="navbar-brand" href="/"><img src="/assets/images/logosmall.jpg"/></a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+			<Navbar>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="/"><img src="/assets/images/logosmall.jpg"/></a>
+    </Navbar.Brand>
+	<Navbar.Toggle/>
+  </Navbar.Header>
+  <Navbar.Collapse>
+  <Nav>
+    <NavItem eventKey={1} href="/register">
+      Register
+    </NavItem>
+    <NavItem eventKey={2} href="/login">
+      Login
+    </NavItem>
+	<NavItem eventKey={3} href="/about">
+      About
+    </NavItem>
+	<NavItem eventKey={4} href="/contactus">
+      Contact Us
+    </NavItem>
+    {/* <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+      <MenuItem eventKey={3.1}>Action</MenuItem>
+      <MenuItem eventKey={3.2}>Another action</MenuItem>
+      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.4}>Separated link</MenuItem>
+    </NavDropdown> */}
+  </Nav>
+</Navbar.Collapse>
+</Navbar>
 
-				<li class="nav-item">
-			      <a class="nav-link" href="/login">LOGIN</a>
-			    </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/about">About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/contactus">Contact Us</a>
-                  </li>
-
-
-                  {/* /* <!-- <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Dropdown link
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                  </li> --> */ }
-                </ul>
-                 <button type="button" class="btn btn-secondary"  onClick={this.handleLogout.bind(this)}>Logout</button>
-
-
-              </div>
-            </nav>
         )
     }
 }
